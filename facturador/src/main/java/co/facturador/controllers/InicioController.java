88c04a7c88c9c951914f4b220d77b916c155f669
eaanillol <anloder4@gmail.com>
2016-03-24 20:08:19 -0500

    Login de usuario completo

--- facturador/src/main/java/co/facturador/controllers/InicioController.java
+++ facturador/src/main/java/co/facturador/controllers/InicioController.java
@@ -2,33 +2,45 @@ package co.facturador.controllers;
 
 import co.com.facturador.services.LoginService;
 import co.com.facturador.services.LoginServiceImpl;
+import co.facturador.controllers.util.UtilController;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 public class InicioController implements Initializable {
     
     @FXML
     private TextField fUsuarioLogin;
     @FXML
-    private TextField fPasswordLogin;
+    private PasswordField fPasswordLogin;
     @FXML
-    private Button bIngresarLogin;
+    private Button bIngresar;
     @FXML
-    private void handleLogin(ActionEvent event) {
+    private void handleLogin(ActionEvent event) {        
         LoginService loginService = new LoginServiceImpl();
-        boolean succes = loginService.loguearUsuario("BABYMADFHS", "02062206");
+       
+        boolean succes = loginService.loguearUsuario(fUsuarioLogin.getText(),fPasswordLogin.getText());
          if(succes){
                System.out.println("Succes");
+               //Obtengo Stage actual para cerra la ventana de login
+               Stage actualStageButton = (Stage) bIngresar.getScene().getWindow();
+               actualStageButton.close();
+               
+               Stage stageDashboard= new Stage();
+               UtilController utilController = new UtilController();
+               utilController.goToScene(stageDashboard, "/fxml/Dashboard.fxml", "/styles/dashboard.css", "Dasboard Usuario::"+fUsuarioLogin.getText());
            }else{
                System.out.println("Not Succes");
            }
-        System.out.println("You clicked me!");
+        
+        
     }
     
     @FXML
@@ -41,7 +53,7 @@ public class InicioController implements Initializable {
                 fPasswordLogin.requestFocus();
             }else if(event.getSource()==fPasswordLogin)
             {
-                bIngresarLogin.requestFocus();
+                bIngresar.requestFocus();
             }
         }
     }
--- facturador/src/main/java/co/facturador/controllers/MainApp.java
+++ facturador/src/main/java/co/facturador/controllers/MainApp.java
@@ -1,26 +1,21 @@
 package co.facturador.controllers;
 
+import co.facturador.controllers.util.UtilController;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 
 public class MainApp extends Application {
 
+    private Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
-        
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
-        
-        stage.setTitle("JavaFX and Maven");
-        stage.setScene(scene);
-        stage.show();
+        this.primaryStage = stage;
+        UtilController utilController = new UtilController();
+        utilController.goToScene(primaryStage,"/fxml/Login.fxml", "/styles/Styles.css", "Login");
     }
+    
 
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
