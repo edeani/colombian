89b5c9bb1b4c7cb72d146c83688f453936704e94
Eder <edeani@eltiempo.com>
2016-06-06 15:25:01 -0500

    proyecto de domicilios spring boot

---
+++ domicilios/src/main/java/com/domicilios/DomiciliosApplication.java
@@ -0,0 +1,12 @@
+package com.domicilios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomiciliosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomiciliosApplication.class, args);
+	}
+}
---
+++ domicilios/src/test/java/com/domicilios/DomiciliosApplicationTests.java
@@ -0,0 +1,18 @@
+package com.domicilios;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DomiciliosApplication.class)
+@WebAppConfiguration
+public class DomiciliosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
