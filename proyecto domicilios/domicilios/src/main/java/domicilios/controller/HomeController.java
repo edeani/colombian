3de18decb223e51e1eb462893bfe2c616c06f7cd
eaanillol <anloder4@gmail.com>
2016-06-29 23:16:10 -0500

    Primera fase proyecto domicilios: Spring mvc,jdbc y perfiles de maven

---
+++ proyecto domicilios/domicilios/src/main/java/domicilios/controller/HomeController.java
@@ -0,0 +1,14 @@
+package domicilios.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+	public ModelAndView inicioDomicilios(){
+		return new ModelAndView("inicio");
+	}
+}
