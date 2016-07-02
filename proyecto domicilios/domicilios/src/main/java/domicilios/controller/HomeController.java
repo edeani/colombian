99c678d31c7ae4dce551882874d1c2eb42bf221b
eaanillol <anloder4@gmail.com>
2016-07-02 13:48:27 -0500

    Implementacion spring security y login base

--- proyecto domicilios/domicilios/src/main/java/domicilios/controller/HomeController.java
+++ proyecto domicilios/domicilios/src/main/java/domicilios/controller/HomeController.java
@@ -2,13 +2,43 @@ package domicilios.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView loginDomicilios(){
+    	ModelAndView mav = new ModelAndView("login");
+		return  mav;
+	}
+	
     @RequestMapping("/")
 	public ModelAndView inicioDomicilios(){
-		return new ModelAndView("inicio");
+    	ModelAndView mav = new ModelAndView("inicio");
+    	mav.addObject("mensaje", "Estoy en Inicio");
+		return  mav;
+	}
+    
+    @RequestMapping("/403")
+    public ModelAndView domicilios403(){
+    	ModelAndView mav = new ModelAndView("inicio");
+    	mav.addObject("mensaje", "Estoy en 403");
+		return  mav;
+	}
+    
+    @RequestMapping("/logoutSuccessful")
+    public ModelAndView domicilioslogout(){
+    	ModelAndView mav = new ModelAndView("inicio");
+    	mav.addObject("mensaje", "Deslogueado");
+		return  mav;
+	}
+    
+    @RequestMapping("/userInfo")
+    public ModelAndView domiciliosuser(){
+    	ModelAndView mav = new ModelAndView("inicio");
+    	mav.addObject("mensaje", "Ingreso Ok");
+		return  mav;
 	}
 }
