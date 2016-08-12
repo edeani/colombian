bd7d8ee1b247eaa1f3a19ad244bcbf4ee454e596
Eder Armando Anillo Lora <edeani@eltiempo.com>
2016-08-11 21:14:38 -0500

    Quito libreria qualifier

--- netbeans/domicilios/src/main/java/domicilios/service/UsuarioServiceImpl.java
+++ netbeans/domicilios/src/main/java/domicilios/service/UsuarioServiceImpl.java
@@ -13,7 +13,6 @@ import domicilios.util.LeerXml;
 import java.util.HashMap;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
