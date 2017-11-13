8d698b72e7c9fea1bef4fe26dc8e105362f63580
eaanillol <anloder4@gmail.com>
2017-11-13 16:54:23 -0500

    Multipath y switch de path en el login

--- netbeans/sede/src/main/java/com/administracion/controller/HomeController.java
+++ netbeans/sede/src/main/java/com/administracion/controller/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
  * @author user
  */
 @Controller
+@RequestMapping()
 public class HomeController{
     
     
--- netbeans/sede/src/main/java/com/administracion/controller/LoginController.java
+++ netbeans/sede/src/main/java/com/administracion/controller/LoginController.java
@@ -5,7 +5,14 @@
  */
 package com.administracion.controller;
 
+import com.administracion.entidad.Sedes;
+import com.administracion.service.SedesService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,14 +23,40 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LoginController {
 
+    @Value("${basededatos}")
+    private String base_datos_principal;
+    
+    @Autowired
+    private SedesService sedesService;
+    
     @RequestMapping(value = "/signin.htm")
-    public ModelAndView paginaLogin() {
+    public ModelAndView paginaLogin(HttpSession session) {
+        session.setAttribute("path", base_datos_principal);
         return new ModelAndView("index");
     }
     
+    @RequestMapping(value = "/{sede:[a-zA-Z]+}/signin.htm")
+    public ModelAndView paginaLoginSede(HttpServletRequest request,@PathVariable String sede) {
+        HttpSession session = request.getSession();
+        Sedes s = sedesService.findSedeXName(sede);
+        if(s==null){
+            return new ModelAndView("redirect:/404.htm");
+        }else{
+            request.getSession().setAttribute("path",sede);
+        }
+        
+       return new ModelAndView("index");
+    }
+    
     @RequestMapping("/403.htm")
-    public ModelAndView domicilios403() {
+    public ModelAndView sedes403() {
         ModelAndView mav = new ModelAndView("403");
+        return mav;
+    }
+    
+    @RequestMapping("/404.htm")
+    public ModelAndView  notfound() {
+        ModelAndView mav = new ModelAndView("404");
         return mav;
     }
 }
