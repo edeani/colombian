f6e8578dd9d538f98d320236362dfdf14418fa57
eaanillol <anloder4@gmail.com>
2017-11-18 14:22:06 -0500

    Actualizar Base controller al de Cucuta

--- netbeans/sede/src/main/java/com/administracion/controller/BaseController.java
+++ netbeans/sede/src/main/java/com/administracion/controller/BaseController.java
@@ -5,9 +5,12 @@
  */
 package com.administracion.controller;
 
+import com.administracion.util.LectorPropiedades;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -16,8 +19,46 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class BaseController {
     
-    private static final String PROPIEDADES_COLOMBIAN="colombian.properties";
+    private final static Logger LOGGER = LoggerFactory.getLogger(BaseController.class);
+    private static String propiedadPrincipal="basedatos";
+    private static String archivo="/bd/basedatos.properties";
+    private LectorPropiedades propiedades;
     
+    public BaseController(){
+        propiedades = new LectorPropiedades();
+        propiedades.setArchivo(getArchivo());
+        propiedades.setPropiedad(getPropiedadPrincipal());
+    }
+    /**
+     * @return the propiedadPrincipal
+     */
+    public static String getPropiedadPrincipal() {
+        return propiedadPrincipal;
+    }
+
+    /**
+     * @param aPropiedadPrincipal the propiedadPrincipal to set
+     */
+    public static void setPropiedadPrincipal(String aPropiedadPrincipal) {
+        propiedadPrincipal = aPropiedadPrincipal;
+    }
+
+    /**
+     * @return the archivo
+     */
+    public static String getArchivo() {
+        return archivo;
+    }
+
+    /**
+     * @param aArchivo the archivo to set
+     */
+    public static void setArchivo(String aArchivo) {
+        archivo = aArchivo;
+    }
+
+    //TODO: agregar manejo de excepciones particulares (no RuntimeException)
+
     private String getEntityKey(Object entity) {
         return StringUtils.uncapitalize(entity.getClass().getSimpleName());
     }
@@ -27,9 +68,19 @@ public class BaseController {
         mav.addObject(entityKey, entity);
         mav.addObject("commandName", entityKey);
     }
-    
-    public static String getPROPIEDADES_COLOMBIAN() {
-        return PROPIEDADES_COLOMBIAN;
+
+    /**
+     * @return the propiedades
+     */
+    public LectorPropiedades getPropiedades() {
+        return propiedades;
+    }
+
+    /**
+     * @param propiedades the propiedades to set
+     */
+    public void setPropiedades(LectorPropiedades propiedades) {
+        this.propiedades = propiedades;
     }
     
 }
