04a688b1ec8e7e9623831ad814c61f5af297c7db
eaanillol <anloder4@gmail.com>
2018-06-09 16:31:06 -0500

    Ajuste de contrsaeña para el perfil de producción administracion

---
+++ netbeans/administracion/src/main/java/PruebasClase1Controller.java
@@ -0,0 +1,18 @@
+
+import org.springframework.stereotype.Controller;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author EderArmando
+ */
+@Controller
+public class PruebasClase1Controller {
+    
+    
+}
