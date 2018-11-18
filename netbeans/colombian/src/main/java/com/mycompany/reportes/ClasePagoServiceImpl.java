c86e1b8209032d3d1c1ac8b88736e4840382239f
Eder Armando Anillo Lora <edeani@eltiempo.com>
2018-11-18 14:10:16 -0500

    <Colombian> Arreglo final de persistencias y JPA

--- netbeans/colombian/src/main/java/com/mycompany/reportes/ClasePagoServiceImpl.java
+++ netbeans/colombian/src/main/java/com/mycompany/reportes/ClasePagoServiceImpl.java
@@ -8,6 +8,11 @@ package com.mycompany.reportes;
 import com.mycompani.bean.util.UserSessionBean;
 import com.mycompany.dao.ClasePagoJpaController;
 import com.mycompany.entidades.ClasePago;
+import com.mycompany.util.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -18,30 +23,57 @@ import javax.persistence.TemporalType;
  *
  * @author EderArmando
  */
-public class ClasePagoServiceImpl implements ClasePagoService{
+public class ClasePagoServiceImpl implements ClasePagoService {
 
     private ClasePagoJpaController clasePagoJpa;
     private final UserSessionBean user = UserSessionBean.getInstance();
     private final String password = user.getSede().getPassword();
+
     @Override
     public boolean pagoServiceXSede(String tipoPago) {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory(user.getNombrePersistencia());
-        EntityManager em = factory.createEntityManager();
-        
-        clasePagoJpa = new  ClasePagoJpaController(factory);
+
         ClasePago clasePago = null;
-        try {
-           String consulta = "SELECT c FROM ClasePago c WHERE c.idsede = :idsede and c.descripcion = :tipoPago";
-           Query query = em.createQuery(consulta);
-           query.setParameter("tipoPago", tipoPago);
-           query.setParameter("idsede", user.getSede().getSed_cod());
-           clasePago =  (ClasePago) query.getSingleResult();
-        }catch(Exception e){
-            System.out.println("ERROR pagoServiceXSede::"+e.getMessage());
-        } finally {
-            em.close();
+        Connection connection;
+        Conexion conexion = new Conexion();
+        conexion.setUser(user.getSede().getUsuario());
+        if (password == null) {
+            conexion.setPassword("");
+        } else {
+            conexion.setPassword(password);
         }
-        return clasePago!=null;
+        conexion.setServer(user.getSede().getIdentificador() + "/" + user.getSede().getBd());
+        conexion.establecerConexion();
+        connection = conexion.getConexion();
+
+        if (connection != null) {
+            ResultSet rs = null;
+            Statement st = null;
+            try {
+
+                String consulta = "SELECT c.* FROM clase_pago c WHERE c.idsede = " + user.getSede().getSed_cod() + " and c.descripcion = '" + tipoPago + "'";
+
+                st = connection.createStatement();
+                PreparedStatement ps = connection.prepareStatement(consulta);
+
+                // ps.setDate(1, d);
+                rs = ps.executeQuery();
+
+                while (rs.next()) {
+                    clasePago = new ClasePago();
+                    clasePago.setId(rs.getInt("id"));
+                    clasePago.setDescripcion("descripcion");
+                    clasePago.setIdsede(rs.getInt("idsede"));
+                }
+               
+            } catch (Exception e) {
+                System.out.println("ERROR pagoServiceXSede::" + e.getMessage());
+            } finally {
+                conexion.cerrar(rs);
+                conexion.cerrar(st);
+                conexion.destruir();
+            }
+        }
+        return clasePago != null;
     }
-    
+
 }
